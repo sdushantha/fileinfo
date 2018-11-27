461ff4c7eb9d03b2f50afa45a87938dc5443004f
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-11-27 08:08:32 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name='file-info',
+  version='1.0',
+  author='Siddharth Dushantha',
+  author_email='siddharth.dushantha@gmail.com',
+  description='Get information on over 10,000 file extensions right from the terminal',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='http://https://github.com/sdushantha/fileinfo',
+  packages=setuptools.find_packages(),
+  scripts=['fileinfo/fileinfo'],
+  install_requires=['requests']
+)
+
